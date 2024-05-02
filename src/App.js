e68fd5b0671e5e6cccb4a3eb8233545b8d548c10
PapaYawAsiamah <99272425+PapaYawAsiamah@users.noreply.github.com>
2024-05-02 02:08:40 +0000

    init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      hello
+    <div className="courses-Container">
+     <h1 className="Heading">Please select a course</h1>
+      <div className="course-Container">
+      
+        <div className="course">
+          <img src="./acesLogo.jpeg" alt="computer enginerring logo" />
+          <h2>computer</h2>
+          <h2>Engineering</h2>
+        </div>
+        <div className="course">
+          <img src="./acesLogo.jpeg" alt="computer enginerring logo" />
+          <h2>computer</h2>
+          <h2>Engineering</h2>
+        </div>
+        <div className="course">
+          <img src="./acesLogo.jpeg" alt="computer enginerring logo" />
+          <h2>computer</h2>
+          <h2>Engineering</h2>
+        </div>
+        <div className="course">
+          <img src="./acesLogo.jpeg" alt="computer enginerring logo" />
+          <h2>computer</h2>
+          <h2>Engineering</h2>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,135 @@
-import React from 'react'
+import React from "react";
+import "../Navbar.css";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useState } from "react";
 
 const Navbar = () => {
-  return (
-    <div>
-      hhh
-    </div>
-  )
-}
+  const [showNav, setShowNav] = useState(false);
+  const [showYearDrop, setShowYearDrop] =useState(false)
 
-export default Navbar
+  const handleHamburgerClick = () => {
+    setShowNav(!showNav);
+  };
+  const showYearDropDown = () => {
+    setShowYearDrop(!showYearDrop)
+  }
+  return (
+    <>
+      <div className="nav">
+        <div>
+          <div className="Address">
+            <img src="./acesLogo.jpeg" alt="aceslogo" />
+
+            <h3 className="poppins-regular">Hi, Logical883</h3>
+          </div>
+
+          <p className="email">logical@gmail.com</p>
+
+          <div className="hamburger-icon">
+            <MenuIcon
+              onClick={handleHamburgerClick}
+              sx={{ color: "white", fontSize: 30, marginLeft: 0 }}
+            />
+          </div>
+        </div>
+        <div>
+          <ul className="mainLinks">
+            <li>
+              <a href="#">Select Year🔻</a>
+              <ul className="dropdown">
+                <li>
+                  <a href="#">Year 1</a>
+                </li>
+                <li>
+                  <a href="#">Year 2</a>
+                </li>
+                <li>
+                  <a href="#">Year 3</a>
+                </li>
+                <li>
+                  <a href="#">Year 4</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href="#">More Info🔻</a>
+              <ul className="dropdown">
+                <li>
+                  <a href="#">Team</a>
+                </li>
+                <li>
+                  <a href="#">History</a>
+                </li>
+                <li>
+                  <a href="#">Mission</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href="#">User Instructions🔻</a>
+              {/* <ul className="dropdown">
+            <li><a href="#">Web Development</a></li>
+            <li><a href="#">Graphic Design</a></li>
+            <li><a href="#">Consulting</a></li>
+          </ul> */}
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className={`sidenav ${showNav ? "active" : ""}`}>
+        <div className="sideAddress">
+          <img src="./acesLogo.jpeg" alt="aceslogo" />
+          <h3 className="poppins-regular">Hi, Logical883</h3>
+        </div>
+        <p className="email">logical@gmail.com</p>
+
+        <ul className="sideLinks">
+          <li>
+            <a href="#"  onClick={showYearDropDown }>🔻Select Year</a>
+            <ul className={`sidedropdown ${showYearDrop ? "active" : ""}`}>
+              <li>
+                <a href="#">Year 1</a>
+              </li>
+              <li>
+                <a href="#">Year 2</a>
+              </li>
+              <li>
+                <a href="#">Year 3</a>
+              </li>
+              <li>
+                <a href="#">Year 4</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="#">🔻More Info</a>
+            <ul className="dropdown">
+              <li>
+                <a href="#">Team</a>
+              </li>
+              <li>
+                <a href="#">History</a>
+              </li>
+              <li>
+                <a href="#">Mission</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="#">🔻User Instructions</a>
+            {/* <ul className="dropdown">
+            <li><a href="#">Web Development</a></li>
+            <li><a href="#">Graphic Design</a></li>
+            <li><a href="#">Consulting</a></li>
+          </ul> */}
+          </li>
+        </ul>
+        <button className="logout-button">
+          logout
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
